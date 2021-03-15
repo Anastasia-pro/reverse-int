87bc95f1ed1eafa62158c6f4b2f48d2d3dbe6cf9
Anastasia-pro <78647195+Anastasia-pro@users.noreply.github.com>
2021-03-15 23:04:03 +0300

    init: start Reverse Int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+  return (
+        parseFloat(
+            num
+                .toString()
+                .split('')
+                .reverse()
+                .join('')
+        ) * Math.sign(num));
+
+};
